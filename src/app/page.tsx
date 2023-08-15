d70b795845042df705756b8c195e29902dd3fbe9
dankichi24 <dan24.yukito@gmail.com>
2023-08-16 07:04:23 +0900

    📝 components作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,17 @@
 import Image from "next/image";
+import AddTask from "./components/AddTask";
+import TodoList from "./components/TodoList";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <main>
+      <h1>Next.js 13 Todo App</h1>
+      <div>
+        <div>
+          <AddTask />
+          <TodoList />
+        </div>
+      </div>
+    </main>
+  );
 }
