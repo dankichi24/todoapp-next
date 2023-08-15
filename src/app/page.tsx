d7a779e56d3921955232af3ac38868f442e24079
dankichi24 <dan24.yukito@gmail.com>
2023-08-16 07:21:43 +0900

    💄 page.tsx HTMLの作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,12 @@ import TodoList from "./components/TodoList";
 
 export default function Home() {
   return (
-    <main>
-      <h1>Next.js 13 Todo App</h1>
-      <div>
-        <div>
+    <main className="flex flex-col items-center justify-center min-h-screen py-2 bg-gray-100">
+      <h1 className="text-4x1 font-bold text-gray-800 -mt-32">
+        Next.js 13 Todo App
+      </h1>
+      <div className="w-full max-w-xl mt-5">
+        <div className="w-full px-8 py-6 bg-white shadow-md rounded-lg">
           <AddTask />
           <TodoList />
         </div>
